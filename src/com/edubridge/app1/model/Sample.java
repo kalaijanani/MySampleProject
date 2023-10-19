6d39883f80368dcf00c512c5ef6a3c6bfc34ea9a
ASUS <ASUS@DESKTOP-PJHGU49>
2023-10-19 13:46:41 +0530

    sample is added

--- src/com/edubridge/app1/model/Sample.java
+++
@@ -1,5 +0,0 @@
-package com.edubridge.app1.model;
-
-public class Sample {
-
-}
---
+++ src/com/edubridge/app1/model/Teacher.java
@@ -0,0 +1,10 @@
+package com.edubridge.app1.model;
+
+public class Teacher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
